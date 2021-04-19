ef51f1e87f5c2eba14115ef6ff108272b7efc9ce
jereme <jeremejazz15@gmail.com>
2021-04-19 21:31:29 +0800

    update design and functionality

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
+"use strict";
+
+const S = document.querySelector.bind(document);
+
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    let principal = parseFloat(S("#principal").value);
+    let rate =      parseFloat(S("#rate").value) ;
+    let years =     parseInt(S("#years").value);
+    let result =    S("#result");
+
+
+    let interest  = 0;
+    let date = new Date();
+    let total_years = date.getFullYear() + years;
+
+    if(isNaN(principal)){
+        alert("Invalid Principal");
+        return;
+    }else if(isNaN(years)){
+        alert("Invalid Years");
+        return;
+
+    }
+
+    interest = principal * ( rate * 0.01 ) * years;
+    let message = `If you deposit ${principal} <br/>
+                    at an interest rate of ${rate}% <br/>
+                    You will receive an amount of ${interest}<br/>
+                    in the year ${total_years}
+    `;
+    result.innerHTML = message;
 }
-        
+
+
+window.onload = function(){
+
+    S("#rate").oninput = function(){
+
+        S("#rate_val").innerText = this.value;
+    };
+
+
+};
